c2270b84ef4ff6cf17571209137c6aec4d79be71
Michael Lazar <lazar.michael22@gmail.com>
2018-03-23 21:27:59 -0400

    Experimenting with datadog ThreadStats

--- scripts/parse_file.py
+++ scripts/parse_file.py
@@ -1,7 +1,41 @@
+import sys
 import time
 import pprint
 
 import apache_log_parser
+from datadog import ThreadStats
+from datadog.threadstats.reporters import Reporter
+
+
+class LocalReporter(Reporter):
+
+    def flush_metrics(self, metrics):
+        print(metrics)
+
+    def flush_events(self, events):
+        print(events)
+
+
+class LocalThreadStats(ThreadStats):
+    """
+    The Data Dog ThreadStats class is hardcoded to broadcast events to a
+    remote server like Statsd. In our case, we only want the data locally
+    so we can display it on the console.
+    """
+
+    def start(self, *args, **kwargs):
+        out = super().start(*args, **kwargs)
+        self.reporter = LocalReporter()
+        return out
+
+
+stats = LocalThreadStats()
+stats.start(flush_interval=1, roll_up_interval=1)
+stats.increment('foo.bar')
+time.sleep(5)
+
+sys.exit(0)
+
 
 # Using the W3C Common Log Format (https://en.wikipedia.org/wiki/Common_Log_Format)
 # Also see https://httpd.apache.org/docs/2.4/logs.html
